189c925a48d497bf6a43ccf3f5c98a9fa244e5ba
Arashita S P <arashitasp@gmail.com>
2025-02-01 18:02:45 +0530

    Update MEDIA ROOT

--- Pinterest/settings.py
+++ Pinterest/settings.py
@@ -138,11 +138,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, "staticfiles")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
