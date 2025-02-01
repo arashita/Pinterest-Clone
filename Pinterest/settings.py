4aa4bae54da8811539259693b7a8fa941d842c62
Arashita S P <arashitasp@gmail.com>
2025-02-01 20:35:51 +0530

    Update settings

--- Pinterest/settings.py
+++ Pinterest/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     "core",
     "users",
 ]
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "Pinterest.urls"
@@ -139,7 +141,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "users", "static"),
+    os.path.join(BASE_DIR, "core", "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
