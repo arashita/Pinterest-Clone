303bfe4af1af0b53bb2b1c5bd9375dbe2eb66210
Arashita S P <arashitasp@gmail.com>
2025-02-03 16:54:58 +0530

    update settings

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import user_login, register, user_logout,home
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "users"
@@ -9,4 +11,4 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path("home/", home, name="home"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
