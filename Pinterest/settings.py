d11dddc575e2616ee2651b39f1499fbec0e2c785
Arashita S P <arashitasp@gmail.com>
2025-02-03 16:45:39 +0530

    update settings

--- Pinterest/settings.py
+++ Pinterest/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-5%f1%8@t=43vr)hjp*t3uur^xf
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'major-project-pinterest.onrender.com']
 
 
